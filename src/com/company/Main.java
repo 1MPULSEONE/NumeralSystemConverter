1b171816299d79d0419151a7f4170897260429ad
1MPULSEONE <parkhomenko2049@gmail.com>
2020-07-14 20:58:48 +0600

    Project is created

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int numDecimal = 20;
+        String bin = Integer.toBinaryString(numDecimal);
+        System.out.printf("%d is equal to 0b%s",2,bin);
+    }
+}
