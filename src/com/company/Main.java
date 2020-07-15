dc6e4747aab1d3e213488acc5c29714db8d468e0
1MPULSEONE <parkhomenko2049@gmail.com>
2020-07-15 17:48:05 +0600

    Main.java modified. Program support reading the source number in the giving base and converting it to another giving base.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,21 +3,32 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
+    private static long length;
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        long num = scanner.nextLong();
-        int destinationRadix = scanner.nextInt();
-        switch (destinationRadix) {
-            case 2:
-                System.out.printf("0b%s",Long.toBinaryString(num));
-                break;
-            case 8:
-                System.out.printf("0%s",Long.toOctalString(num));
-                break;
-            case 16:
-                System.out.printf("0x%s",Long.toHexString(num));
-                break;
+        int sourceRadix = scanner.nextInt();
+        String sourceNumber = scanner.next();
+        int targetRadix = scanner.nextInt();
+        int targetNumber = Integer.parseInt(sourceNumber);
+        if (sourceRadix == 1) {
+            length = sourceNumber.length();
+            targetNumber = 0;
+            for (long i = 0; i < length; i++) {
+                targetNumber++;
+            }
+        } else {
+            length = Long.parseLong(sourceNumber);
+        }
+        if (sourceRadix != 10 && sourceRadix != 1) {
+            targetNumber = Integer.parseInt(sourceNumber,sourceRadix);
+        }
+        if (targetRadix == 1) {
+            for (long i = 0; i < length; i++) {
+                System.out.print(1);
+            }
+        } else {
+            sourceNumber = Integer.toString(targetNumber,targetRadix);
+            System.out.println(sourceNumber);
         }
     }
 }
